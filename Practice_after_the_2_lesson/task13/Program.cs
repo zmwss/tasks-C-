1c5521724d2813d4fbce8c2a622480c5c3d81949
zmwss <smirnov2003daniil@mail.ru>
2023-03-15 20:56:40 +0300

    Solution of the 13 problem

---
+++ Practice_after_the_2_lesson/task13/Program.cs
@@ -0,0 +1,12 @@
+﻿int third_digit(int number)
+{
+    if(number < 100 || number > 999) return -1;
+    while(number > 999) number/=10;
+    return number%10;
+}
+
+Console.Write("Enter an integer number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int digit = third_digit(Math.Abs(number));
+if(digit == -1) Console.WriteLine($"The number {number} doesn't have a third digit");
+else Console.WriteLine($"The third digit of the number {number} is {digit}");
