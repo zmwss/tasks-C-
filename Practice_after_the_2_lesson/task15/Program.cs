d056dbaecd08250d5b5614cd6f0989f5d6d5207c
zmwss <smirnov2003daniil@mail.ru>
2023-03-15 20:23:06 +0300

    2 lesson

---
+++ Practice_after_the_2_lesson/task15/Program.cs
@@ -0,0 +1,15 @@
+﻿int second_digit(int number)
+{
+    number/=10;
+    return number%10;
+}
+
+Console.Write("Enter three-digit number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(Math.Abs(number)<100 || Math.Abs(number)>999)
+{
+    Console.WriteLine("Enter three-digit number!");
+    return;
+}
+
+else Console.WriteLine($"The second digit of {number} is {second_digit(Math.Abs(number))}");
