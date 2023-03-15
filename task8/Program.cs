a3a29e220986b77886ced06c645639cc2577ad18
zmwss <smirnov2003daniil@mail.ru>
2023-03-15 20:02:35 +0300

    Solution of the problem 8

--- task8/Program.cs
+++ task8/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Enter an natural number N");
+uint N = Convert.ToUInt32(Console.ReadLine());
+ if(N < 2) Console.WriteLine($"There are no even numbers up to {N}");
+ else
+ {
+    Console.Write($"Even numbers from 2 to {N}:");
+     for(int i=2; i <= N; i+=2)
+        {
+            Console.Write($" {i}");
+        }
+ }
