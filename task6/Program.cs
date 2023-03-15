a1edfde735a1c701a3cc93711377177bbe4e14fd
zmwss <smirnov2003daniil@mail.ru>
2023-03-15 16:57:55 +0300

    Solution of problem 6

---
+++ task6/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Enter an integer number a");
+int a = Convert.ToInt32(Console.ReadLine());
+if(a%2 == 0) Console.WriteLine($"The number {a} is even");
+else Console.WriteLine($"The number {a} is odd");
