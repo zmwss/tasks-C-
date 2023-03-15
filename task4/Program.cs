165ae0189dcb2b5450fb073d82e53714853eeeb7
zmwss <smirnov2003daniil@mail.ru>
2023-03-15 16:50:09 +0300

    Solution of problem 4

---
+++ task4/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Enter an number a");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter an number b");
+double b = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter an number c");
+double c = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine($"The maximum of the numbers {a} and {b} and {c}: {(a > b ? (a > c ? a : c) : (b > c ? b : c))}");
