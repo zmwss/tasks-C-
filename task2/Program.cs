52259ed75df51b3eab5e9ea72634b0815da4200b
zmwss <smirnov2003daniil@mail.ru>
2023-03-15 16:36:53 +0300

    Solution of problem 2

---
+++ task2/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Enter an integer number a");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter an integer number b");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"The maximum of the numbers {a} and {b}: {(a > b ? a: b)}");
