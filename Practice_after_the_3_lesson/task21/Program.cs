a290efeb75323e84e52638a88384f96953d545b0
zmwss <smirnov2003daniil@mail.ru>
2023-03-16 16:49:02 +0300

    Solution of the problem 21

---
+++ Practice_after_the_3_lesson/task21/Program.cs
@@ -0,0 +1,23 @@
+﻿double distance(double xA, double yA, double zA,
+                double xB, double yB, double zB)
+{
+    return Math.Sqrt((xA-xB)*(xA-xB) + (yA-yB)*(yA-yB) + (zA-zB)*(zA-zB));
+}
+
+System.Console.WriteLine("Enter the coordinates of point A: ");
+System.Console.Write("x =");
+double xA = Convert.ToDouble(Console.ReadLine());
+System.Console.Write("y =");
+double yA = Convert.ToDouble(Console.ReadLine());
+System.Console.Write("z =");
+double zA = Convert.ToDouble(Console.ReadLine());
+
+System.Console.WriteLine("Enter the coordinates of point B: ");
+System.Console.Write("x =");
+double xB = Convert.ToDouble(Console.ReadLine());
+System.Console.Write("y =");
+double yB = Convert.ToDouble(Console.ReadLine());
+System.Console.Write("z =");
+double zB = Convert.ToDouble(Console.ReadLine());
+
+System.Console.WriteLine($"Distance between points A and B equals {distance(xA, yA, zA, xB, yB, zB)}");
