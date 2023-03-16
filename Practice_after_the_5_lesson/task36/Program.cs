8b9d6d7c2cb741e367601e8ba2fde76f410e0db4
zmwss <smirnov2003daniil@mail.ru>
2023-03-16 18:48:29 +0300

    Solution of the problem 36

---
+++ Practice_after_the_5_lesson/task36/Program.cs
@@ -0,0 +1,39 @@
+﻿void PrintArray(int [] arr)
+{
+    int length = arr.Length;
+    int i = 0;
+    while(i < length)
+    {
+        System.Console.Write($"{arr[i]} ");
+        i++;
+    }
+    System.Console.WriteLine();
+}
+
+void FillArray(int[] arr)
+{
+    int length = arr.Length;
+    int i = 0;
+    while (i < length)
+    {
+        arr[i] = new Random().Next(-100, 101);
+        i++;
+    }
+}
+
+int SumOddIndex(int[] arr)
+{
+    int sum = 0;
+    int length = arr.Length;
+    for(int i = 1; i < length; i+=2)
+    {
+        sum+=arr[i];
+    }
+    return sum;
+}
+
+int [] array = new int[4];
+FillArray(array);
+PrintArray(array);
+System.Console.WriteLine($"The sum of the elements in odd positions: {SumOddIndex(array)}");
+
