61f7581ee6415d749b9c3c6a4d7e7669e31cb222
zmwss <smirnov2003daniil@mail.ru>
2023-03-16 17:40:26 +0300

    Solution of the problem 27

---
+++ Practice_after_the_4_lesson/task27/Program.cs
@@ -0,0 +1,14 @@
+﻿int sum_digits(int n)
+{
+    int sum = 0;
+    while(n > 0)
+    {
+        sum+=n%10;
+        n/=10;
+    }
+    return sum;
+}
+
+System.Console.WriteLine("Enter a number n");
+int n = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine($"The sum of the digits of the {n} equals {sum_digits(Math.Abs(n))}");
