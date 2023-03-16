450e92250278a0dbf051cc5639fe13d87f8aec80
zmwss <smirnov2003daniil@mail.ru>
2023-03-16 17:53:25 +0300

    Solution of the problem 29

---
+++ Practice_after_the_4_lesson/task29/Program.cs
@@ -0,0 +1,26 @@
+﻿void FillArray(int[] arr)
+{
+    int length = arr.Length;
+    int i = 0;
+    Random rnd = new Random();
+    while (i < length)
+    {
+        arr[i] = rnd.Next();
+        i++;
+    }
+}
+
+void PrintArray(int [] arr)
+{
+    int length = arr.Length;
+    int i = 0;
+    while(i < length)
+    {
+        System.Console.Write($"{arr[i]} ");
+        i++;
+    }
+}
+
+int [] array = new int[8];
+FillArray(array);
+PrintArray(array);
