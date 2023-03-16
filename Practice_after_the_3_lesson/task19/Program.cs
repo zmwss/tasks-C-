a8fa25e67c0a3e04b4048b4fa590b88bb50e2ddf
zmwss <smirnov2003daniil@mail.ru>
2023-03-16 16:35:28 +0300

    Solution of the problem 19

---
+++ Practice_after_the_3_lesson/task19/Program.cs
@@ -0,0 +1,11 @@
+﻿bool palindrome(int number)
+{
+    return (number/10000 == number%10 && number/1000%10 == number/10%10);
+}
+
+System.Console.WriteLine("Enter a five-digit number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int m = Math.Abs(n);
+if(m < 10000 || m > 99999) System.Console.WriteLine("Enter a five-digit number!");
+else if(palindrome(m)) System.Console.WriteLine($"The number {n} is a palindrome");
+else System.Console.WriteLine($"The number {n} is not a palindrome");
