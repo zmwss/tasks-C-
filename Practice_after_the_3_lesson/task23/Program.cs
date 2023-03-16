0a4fe6b505b8a0329aed806d9814bac0b39d9c2f
zmwss <smirnov2003daniil@mail.ru>
2023-03-16 16:58:26 +0300

    Solution of the problem 23

---
+++ Practice_after_the_3_lesson/task23/Program.cs
@@ -0,0 +1,14 @@
+﻿void print_cubes(int N)
+{
+    for(int i = 1; i <= N; i++)
+    Console.Write($"{i*i*i} ");
+}
+
+System.Console.WriteLine("Enter a natural number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if(n < 1) System.Console.WriteLine("Enter a natural number!");
+else 
+{
+    System.Console.Write($"Cubes from 1 to {n}: ");
+    print_cubes(n);
+}
