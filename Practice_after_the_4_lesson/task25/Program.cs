57930bd54794036d92b407b8d77d2eeec1d44c39
zmwss <smirnov2003daniil@mail.ru>
2023-03-16 17:31:55 +0300

    Solution of the problem 25

---
+++ Practice_after_the_4_lesson/task25/Program.cs
@@ -0,0 +1,16 @@
+﻿System.Console.WriteLine("Enter a number A");
+double a = Convert.ToDouble(Console.ReadLine());
+System.Console.WriteLine("Enter a natural number B");
+int b = Convert.ToInt32(Console.ReadLine());
+if(b < 1)
+{
+    System.Console.WriteLine("Enter a natural number B!");
+    return;
+}
+double aCopy = a;
+for(int i = 1; i < b; i++)
+{
+    a*=aCopy;
+}
+
+System.Console.WriteLine($"A to the power of B: {a}");
