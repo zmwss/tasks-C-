d2c7d87bcf2a1fdd34d02b0e69323dda19bc123e
zmwss <smirnov2003daniil@mail.ru>
2023-03-16 19:05:33 +0300

    Solution of the problem 38

---
+++ Practice_after_the_5_lesson/task38/Program.cs
@@ -0,0 +1,16 @@
+﻿double DiffMaxMin(double [] arr)
+{
+    double max = arr[0];
+    double min = arr[0];
+    int length = arr.Length;
+    for(int i = 1; i < length; i++)
+    {
+        if(max < arr[i]) max = arr[i];
+        if(min > arr[i]) min = arr[i];
+    }
+    return max-min;
+}
+
+double[] arr = {2.5, 5.4, 7.0, -5.4, 25.3};
+if(arr.Length == 0) System.Console.WriteLine("The array is empty");
+else System.Console.WriteLine($"Difference between max and min: {DiffMaxMin(arr)}");
