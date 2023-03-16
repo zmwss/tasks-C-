bdcaae574b2c19d6b45fde31981dee80d803d834
zmwss <smirnov2003daniil@mail.ru>
2023-03-16 18:26:02 +0300

    Solution of the problem 34

---
+++ Practice_after_the_5_lesson/task34/Program.cs
@@ -0,0 +1,38 @@
+﻿int NumberOfEven(int [] array)
+{
+    int number = 0;
+    int length = array.Length;
+    for(int i = 0; i < length; i++)
+    {
+        if(array[i]%2 == 0) number+=1;
+    }
+    return number;
+}
+
+void PrintArray(int [] arr)
+{
+    int length = arr.Length;
+    int i = 0;
+    while(i < length)
+    {
+        System.Console.Write($"{arr[i]} ");
+        i++;
+    }
+    System.Console.WriteLine();
+}
+
+void FillArray(int[] arr)
+{
+    int length = arr.Length;
+    int i = 0;
+    while (i < length)
+    {
+        arr[i] = new Random().Next(100, 1000);
+        i++;
+    }
+}
+
+int[] arr = new int[10];
+FillArray(arr);
+PrintArray(arr);
+System.Console.WriteLine($"Number of even elements: {NumberOfEven(arr)}");
