36c5c81e0b5d16599b315bb233e4116d2d312bed
zmwss <smirnov2003daniil@mail.ru>
2023-03-18 20:24:43 +0300

    Solution of the problem 47

---
+++ Practice_after_the_7_lesson/task47/Program.cs
@@ -0,0 +1,28 @@
+﻿void PrintArray(double[,] arr)
+{
+    for(int i = 0; i < arr.GetLength(0); i++)
+    {
+        for(int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(double[,] arr)
+{
+    Random rand = new Random();
+    for(int i = 0; i < arr.GetLength(0); i++)
+    {
+        for(int j = 0; j < arr.GetLength(1); j++)
+            arr[i,j] = rand.NextDouble()*10;
+    }
+}
+Console.WriteLine("Enter the number of rows");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the number of columns");
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] array = new double[m,n];
+FillArray(array);
+PrintArray(array);
