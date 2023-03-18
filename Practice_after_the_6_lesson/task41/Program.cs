e5165f26975682fe47d082200aba62da4c8aff34
zmwss <smirnov2003daniil@mail.ru>
2023-03-18 03:06:38 +0300

    Solution of the problem 41

---
+++ Practice_after_the_6_lesson/task41/Program.cs
@@ -0,0 +1,24 @@
+﻿int[] InputArray(int length)
+{
+    int[] array = new int[length];
+    for(int i = 0; i < length; i++)
+    {
+        Console.WriteLine($"Enter the {i}-th number");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+int PositiveNumbers(int [] arr)
+{
+    int number = 0;
+    int length = arr.Length;
+    for(int i = 0; i < length; i++)
+    if(arr[i] > 0) number++;
+    return number;
+}
+
+Console.WriteLine("Enter the number of numbers");
+int length = Convert.ToInt32(Console.ReadLine());
+int number = PositiveNumbers(InputArray(length));
+Console.WriteLine($"You have entered {number} numbers greater than zero");
