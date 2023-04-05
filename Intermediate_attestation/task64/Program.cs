8433fc869feabada0b3151c67ff18668bfab5586
zmwss <smirnov2003daniil@mail.ru>
2023-04-05 19:59:23 +0300

    Solution of the problem 64

---
+++ Intermediate_attestation/task64/Program.cs
@@ -0,0 +1,18 @@
+﻿void PrintNumbers(int N)
+{
+    Console.Write($"{N} ");
+    if(N > 1) PrintNumbers(N-1);
+}
+Console.Write("Enter a natural number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if(n < 1)
+{
+    Console.WriteLine("You entered non-natural number.");
+    return;
+}
+if(n > 100)
+{
+    Console.WriteLine("You entered too large a number for this task.");
+    return;
+}
+PrintNumbers(n);
