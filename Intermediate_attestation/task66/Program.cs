8bce7fdb13898f3e8d59269671bda2d90b264c6e
zmwss <smirnov2003daniil@mail.ru>
2023-04-05 20:26:27 +0300

    Solution of the problem 66

---
+++ Intermediate_attestation/task66/Program.cs
@@ -0,0 +1,27 @@
+﻿int SumNumbersFromMtoN(int M, int N)
+{
+    if(M > N) return 0;
+    else if(M==N) return M;
+    return M+N+SumNumbersFromMtoN(M+1, N-1);
+}
+
+Console.Write("Enter a natural number M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+if(M < 1)
+{
+    Console.WriteLine("You entered non-natural number.");
+    return;
+}
+Console.Write("Enter a natural number N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+if(N < 1)
+{
+    Console.WriteLine("You entered non-natural number.");
+    return;
+}
+if(N-M > 10000)
+{
+    Console.WriteLine($"Difference beetween {N} and {M} too large for this task.");
+    return;
+}
+Console.WriteLine($"The sum of the numbers from {M} to {N} {SumNumbersFromMtoN(M, N)}");
